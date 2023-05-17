296839e70a377c8a394db1b58a171748aa9d2e87
inhegass <112832963+inhegass@users.noreply.github.com>
2023-05-17 08:10:13 -0300

    Update 33A_JOAOVITORVILLA_2212585_ArquivoVetStr.c

--- 33A_JOAOVITORVILLA_2212585_ArquivoVetStr.c
+++ 33A_JOAOVITORVILLA_2212585_ArquivoVetStr.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 
+void alocaMemoria(char** v, int n1, int n2) {
+	for (int i = 0; i < n1; i++) {
+		v[i] = (char**)malloc(n2 * sizeof(char*));
+		if (v[i] == NULL) {
+			printf("Memoria insuficiente.\n");
+			exit(1);
+		}
+	}
+	return;
+}
+
 int main(void) {
 	FILE* f;
-	char* listaAlfab[26];
-	char** obras;
-	char** autores;
+
+	char alfab[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char* obras[100];
+	char* autores[100];
 	f = fopen("livros.txt", "r");
 	if (f == NULL) {
 		printf("Erro na abertura do arquivo.\n");
 		exit(1);
-		while (!feof(f)) {
-
-		}
+	}
+	alocaMemoria(obras, 100, 127);
+	alocaMemoria(autores, 100, 127);
+	while (!feof(f)) {
+		fscanf
 	}
 }
-
-//vai ter que usar alocação dinamica nos vetores
